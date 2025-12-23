da4e16d80e6347b28240551b1743cff8a5b3dc2e
Naveen Tehrpariya <naveentehrpariya@MacBook-Air-2.local>
2025-12-23 11:23:33 +0530

    merge

--- pages/why-us.js
+++ pages/why-us.js
@@ -31,7 +31,7 @@ export default function WhyUs() {
                     priority 
                     fetchPriority="high"
                     sizes="100vw"
-                    quality={30}
+                    quality={40}
                     style={{ objectFit: 'cover', zIndex: -1 }}
                     placeholder="blur"
                     blurDataURL={getImages('about-hero.webp?quality=1')}
